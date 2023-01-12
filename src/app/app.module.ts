4805974e2bf60bf760b1bbd53b8301d6d0b5a702
Cyril6978 <cyril.david8978@gmail.com>
2023-01-12 13:46:53 +0100

    my solution

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { ROUTES } from "./app.route";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    UserProfileComponent,
+    MenuComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
